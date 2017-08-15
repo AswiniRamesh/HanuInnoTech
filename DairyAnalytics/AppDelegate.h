cd0e37f395e0ebadbe64b436b0041a376df0ac24
Aswini Ramesh <Aswini@Naveens-MacBook-Pro.local>
2017-08-15 12:50:13 -0700

    Initial Commit

---
+++ DairyAnalytics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DairyAnalytics
+//
+//  Created by Aswini Ramesh on 8/15/17.
+//  Copyright © 2017 Aswini Ramesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DairyAnalytics/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DairyAnalytics
+//
+//  Created by Aswini Ramesh on 8/15/17.
+//  Copyright © 2017 Aswini Ramesh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
